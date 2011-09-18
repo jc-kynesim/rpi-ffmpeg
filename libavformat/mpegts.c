6f9ffd7f568f12b83ebb8a9249dc710426b24cc2
Joakim Plate <elupus@ecce.se>
2011-09-18 19:16:34 +0200

    Don't accept mpegts PMT that isn't current

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -552,6 +552,7 @@ typedef struct SectionHeader {
     uint8_t tid;
     uint16_t id;
     uint8_t version;
+    uint8_t current;
     uint8_t sec_num;
     uint8_t last_sec_num;
 } SectionHeader;
@@ -623,6 +624,7 @@ static int parse_section_header(SectionHeader *h,
     val = get8(pp, p_end);
     if (val < 0)
         return val;
+    h->current = val & 0x1;
     h->version = (val >> 1) & 0x1f;
     val = get8(pp, p_end);
     if (val < 0)
@@ -1890,6 +1892,8 @@ static void pat_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
     if (h->tid != PAT_TID)
         return;
     if (ts->skip_changes)
+        return;
+    if (!h->current)
         return;
 
     ts->stream->ts_id = h->id;
