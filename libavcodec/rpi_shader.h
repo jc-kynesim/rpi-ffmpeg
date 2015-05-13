36aba6ea897093f6528658e78bf4deeba7eeecd2
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-13 15:13:47 +0100

    Added B prediction - not quite right

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -6,8 +6,8 @@ extern unsigned int rpi_shader[];
 #define mc_setup_uv (rpi_shader + 0)
 #define mc_filter_uv (rpi_shader + 142)
 #define mc_filter_uv_b (rpi_shader + 360)
-#define mc_exit (rpi_shader + 588)
-#define mc_interrupt_exit8 (rpi_shader + 606)
-#define mc_end (rpi_shader + 636)
+#define mc_exit (rpi_shader + 592)
+#define mc_interrupt_exit8 (rpi_shader + 610)
+#define mc_end (rpi_shader + 640)
 
 #endif
