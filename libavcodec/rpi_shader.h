e48df43c16de74dddbc7c702d64dd01eaf8e6b39
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-14 11:17:09 +0100

    Used P delay slots more efficiently

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -5,10 +5,10 @@ extern unsigned int rpi_shader[];
 
 #define mc_setup_uv (rpi_shader + 0)
 #define mc_filter_uv (rpi_shader + 152)
-#define mc_filter_uv_b0 (rpi_shader + 324)
-#define mc_filter_uv_b (rpi_shader + 490)
-#define mc_exit (rpi_shader + 670)
-#define mc_interrupt_exit8 (rpi_shader + 688)
-#define mc_end (rpi_shader + 718)
+#define mc_filter_uv_b0 (rpi_shader + 318)
+#define mc_filter_uv_b (rpi_shader + 484)
+#define mc_exit (rpi_shader + 664)
+#define mc_interrupt_exit8 (rpi_shader + 682)
+#define mc_end (rpi_shader + 712)
 
 #endif
