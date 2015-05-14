29956c5549eb94e418c42e838d0bfceeb95730b0
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-14 11:31:23 +0100

    Avoid writeback of first B results

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -6,9 +6,9 @@ extern unsigned int rpi_shader[];
 #define mc_setup_uv (rpi_shader + 0)
 #define mc_filter_uv (rpi_shader + 152)
 #define mc_filter_uv_b0 (rpi_shader + 316)
-#define mc_filter_uv_b (rpi_shader + 476)
-#define mc_exit (rpi_shader + 650)
-#define mc_interrupt_exit8 (rpi_shader + 668)
-#define mc_end (rpi_shader + 698)
+#define mc_filter_uv_b (rpi_shader + 466)
+#define mc_exit (rpi_shader + 640)
+#define mc_interrupt_exit8 (rpi_shader + 658)
+#define mc_end (rpi_shader + 688)
 
 #endif
