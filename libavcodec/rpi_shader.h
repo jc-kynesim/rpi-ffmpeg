6bbb5df80c069d8fe2ab11907074a285f62b99b6
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-14 11:12:43 +0100

    Optimised B pass

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -7,8 +7,8 @@ extern unsigned int rpi_shader[];
 #define mc_filter_uv (rpi_shader + 152)
 #define mc_filter_uv_b0 (rpi_shader + 324)
 #define mc_filter_uv_b (rpi_shader + 490)
-#define mc_exit (rpi_shader + 718)
-#define mc_interrupt_exit8 (rpi_shader + 736)
-#define mc_end (rpi_shader + 766)
+#define mc_exit (rpi_shader + 670)
+#define mc_interrupt_exit8 (rpi_shader + 688)
+#define mc_end (rpi_shader + 718)
 
 #endif
