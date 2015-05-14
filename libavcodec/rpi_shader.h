bdd83b5be5d8e82763c08e4e520544ac36a127f8
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-14 09:42:16 +0100

    Split B prediction into two passes

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -4,10 +4,11 @@
 extern unsigned int rpi_shader[];
 
 #define mc_setup_uv (rpi_shader + 0)
-#define mc_filter_uv (rpi_shader + 142)
-#define mc_filter_uv_b (rpi_shader + 360)
-#define mc_exit (rpi_shader + 592)
-#define mc_interrupt_exit8 (rpi_shader + 610)
-#define mc_end (rpi_shader + 640)
+#define mc_filter_uv (rpi_shader + 150)
+#define mc_filter_uv_b0 (rpi_shader + 368)
+#define mc_filter_uv_b (rpi_shader + 586)
+#define mc_exit (rpi_shader + 818)
+#define mc_interrupt_exit8 (rpi_shader + 836)
+#define mc_end (rpi_shader + 866)
 
 #endif
