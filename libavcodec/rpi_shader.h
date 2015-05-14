6b1643132586fe82c87fbfc597d8c68816bb98cb
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-14 10:55:07 +0100

    P prediction uses 4 tap filters

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -5,10 +5,10 @@ extern unsigned int rpi_shader[];
 
 #define mc_setup_uv (rpi_shader + 0)
 #define mc_filter_uv (rpi_shader + 152)
-#define mc_filter_uv_b0 (rpi_shader + 370)
-#define mc_filter_uv_b (rpi_shader + 584)
-#define mc_exit (rpi_shader + 812)
-#define mc_interrupt_exit8 (rpi_shader + 830)
-#define mc_end (rpi_shader + 860)
+#define mc_filter_uv_b0 (rpi_shader + 324)
+#define mc_filter_uv_b (rpi_shader + 538)
+#define mc_exit (rpi_shader + 766)
+#define mc_interrupt_exit8 (rpi_shader + 784)
+#define mc_end (rpi_shader + 814)
 
 #endif
