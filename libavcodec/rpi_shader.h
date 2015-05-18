73efb71973efc4ec78d17e81b9061cbefc2428a0
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-18 11:11:02 +0100

    Added support for weighted prediction in P frames

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -4,11 +4,11 @@
 extern unsigned int rpi_shader[];
 
 #define mc_setup_uv (rpi_shader + 0)
-#define mc_filter_uv (rpi_shader + 148)
-#define mc_filter_uv_b0 (rpi_shader + 310)
-#define mc_filter_uv_b (rpi_shader + 458)
-#define mc_exit (rpi_shader + 630)
-#define mc_interrupt_exit8 (rpi_shader + 648)
-#define mc_end (rpi_shader + 678)
+#define mc_filter_uv (rpi_shader + 152)
+#define mc_filter_uv_b0 (rpi_shader + 342)
+#define mc_filter_uv_b (rpi_shader + 494)
+#define mc_exit (rpi_shader + 670)
+#define mc_interrupt_exit8 (rpi_shader + 688)
+#define mc_end (rpi_shader + 718)
 
 #endif
