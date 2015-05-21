791525c3bd6ea49fe00492e1b98de6804f77ef40
popcornmix <popcornmix@gmail.com>
2015-05-21 16:50:02 +0100

    Pass qpu number in as uniform

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -4,15 +4,15 @@
 extern unsigned int rpi_shader[];
 
 #define mc_setup_uv (rpi_shader + 0)
-#define mc_filter_uv (rpi_shader + 148)
-#define mc_filter_uv_b0 (rpi_shader + 338)
-#define mc_filter_uv_b (rpi_shader + 490)
-#define mc_exit (rpi_shader + 666)
-#define mc_interrupt_exit8 (rpi_shader + 684)
-#define mc_setup (rpi_shader + 714)
-#define mc_filter (rpi_shader + 868)
-#define mc_filter_b (rpi_shader + 1108)
-#define mc_interrupt_exit12 (rpi_shader + 1364)
-#define mc_end (rpi_shader + 1402)
+#define mc_filter_uv (rpi_shader + 144)
+#define mc_filter_uv_b0 (rpi_shader + 334)
+#define mc_filter_uv_b (rpi_shader + 486)
+#define mc_exit (rpi_shader + 662)
+#define mc_interrupt_exit8 (rpi_shader + 680)
+#define mc_setup (rpi_shader + 710)
+#define mc_filter (rpi_shader + 864)
+#define mc_filter_b (rpi_shader + 1104)
+#define mc_interrupt_exit12 (rpi_shader + 1360)
+#define mc_end (rpi_shader + 1398)
 
 #endif
