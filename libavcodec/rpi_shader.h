5b887aaa543a10f06b87d7b89b948e79799631b5
Peter de Rivaz <peter.derivaz@gmail.com>
2015-05-27 16:44:29 +0100

    Added support for running luma prediction on QPUs

--- libavcodec/rpi_shader.h
+++ libavcodec/rpi_shader.h
@@ -4,15 +4,16 @@
 extern unsigned int rpi_shader[];
 
 #define mc_setup_uv (rpi_shader + 0)
-#define mc_filter_uv (rpi_shader + 144)
-#define mc_filter_uv_b0 (rpi_shader + 334)
-#define mc_filter_uv_b (rpi_shader + 486)
-#define mc_exit (rpi_shader + 662)
-#define mc_interrupt_exit8 (rpi_shader + 680)
-#define mc_setup (rpi_shader + 710)
-#define mc_filter (rpi_shader + 864)
-#define mc_filter_b (rpi_shader + 1104)
-#define mc_interrupt_exit12 (rpi_shader + 1360)
-#define mc_end (rpi_shader + 1398)
+#define mc_filter_uv (rpi_shader + 130)
+#define mc_filter_uv_b0 (rpi_shader + 312)
+#define mc_filter_uv_b (rpi_shader + 464)
+#define mc_exit (rpi_shader + 640)
+#define mc_interrupt_exit8 (rpi_shader + 658)
+#define mc_setup (rpi_shader + 688)
+#define mc_filter (rpi_shader + 1048)
+#define mc_filter_b (rpi_shader + 1174)
+#define mc_interrupt_exit12 (rpi_shader + 1302)
+#define mc_exit1 (rpi_shader + 1340)
+#define mc_end (rpi_shader + 1356)
 
 #endif
