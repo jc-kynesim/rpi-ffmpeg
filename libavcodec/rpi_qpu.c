f2d830a5fc678cb6a72bd6bf2403f1b0fcb50a78
John Cox <jc@kynesim.co.uk>
2016-01-13 16:13:33 +0000

    H.265 residual decode rework (v2)
    Rework the cabac decode functions
    Simplify the code flow and variable usage where possible
    
    (Remove profiling and other spurious deltas that were in v1)

--- libavcodec/rpi_qpu.c
+++ libavcodec/rpi_qpu.c
@@ -1,3 +1,4 @@
+#ifdef RPI
 // This works better than the mmap in that the memory can be cached, but requires a kernel modification to enable the device.
 // define RPI_TIME_TOTAL_QPU to print out how much time is spent in the QPU code
 //#define RPI_TIME_TOTAL_QPU
@@ -15,6 +16,8 @@
 #include <string.h>
 #include <stddef.h>
 #include "libavutil/avassert.h"
+
+#include "config.h"
 
 #include <pthread.h>
 #include <time.h>
@@ -1215,3 +1218,5 @@ void rpi_do_block(const uint8_t *in_buffer_vc, int src_pitch, uint8_t *dst_vc, i
 
 
 #endif
+
+#endif // RPI
