5b37b7f98e46b58cf20a2a70e65e44d380649697
John Cox <jc@kynesim.co.uk>
2017-05-17 18:12:16 +0100

    Add better usage & TMU stall summaries

--- pi-util/v3dusage.py
+++ pi-util/v3dusage.py
@@ -4,22 +4,22 @@ import sys
 import argparse
 import re
 
-def main():
-    argp = argparse.ArgumentParser(description="QPU/VPU perf summary")
-    argp.add_argument("logfile")
-    args = argp.parse_args()
-
+def do_logparse(logname):
 
     rmatch = re.compile(r'^([0-9]+\.[0-9]{3}): (done )?((vpu0)|(vpu1)|(qpu1)) ([A-Z_]+) cb:([0-9a-f]+) ')
+    rqcycle = re.compile(r'^([0-9]+\.[0-9]{3}): v3d: QPU Total clock cycles for all QPUs doing vertex/coordinate shading +([0-9]+)$')
+    rqtscycle = re.compile(r'^([0-9]+\.[0-9]{3}): v3d: QPU Total clock cycles for all QPUs stalled waiting for TMUs +([0-9]+)$')
 
     ttotal = {'idle':0.0}
     tstart = {}
+    qctotal = {}
+    qtstotal = {}
     time0 = None
     idle_start = None
     qpu_op_no = 0
     op_count = 0
 
-    with open(args.logfile, "rt") as infile:
+    with open(logname, "rt") as infile:
         for line in infile:
             match = rmatch.match(line)
             if match:
@@ -60,6 +60,20 @@ def main():
                     ttotal['idle'] += time - idle_start
                     idle_start = None
 
+            match = rqcycle.match(line)
+            if match:
+                unit = "qpu1." + str(qpu_op_no)
+                if not unit in qctotal:
+                    qctotal[unit] = 0;
+                qctotal[unit] += int(match.group(2))
+
+            match = rqtscycle.match(line)
+            if match:
+                unit = "qpu1." + str(qpu_op_no)
+                if not unit in qtstotal:
+                    qtstotal[unit] = 0;
+                qtstotal[unit] += int(match.group(2))
+
     if not time0:
         print "No v3d profile records found"
     else:
@@ -68,8 +82,31 @@ def main():
         print "Logged time:", tlogged, "  Op count:", op_count
         for unit in sorted(ttotal):
             print b'%6s: %10.3f    %7.3f%%' % (unit, ttotal[unit], ttotal[unit] * 100.0 / tlogged)
+        print
+        for unit in sorted(qctotal):
+            if not unit in qtstotal:
+                qtstotal[unit] = 0;
+            print b'%6s: Qcycles: %10d, TMU stall: %10d  %7.3f%%' % (unit, qctotal[unit], qtstotal[unit], (qtstotal[unit] * 100.0)/qctotal[unit])
+
 
 
 if __name__ == '__main__':
-   main()
+    argp = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="QPU/VPU perf summary from VC logging",
+        epilog = """
+Will also summarise TMU stalls if logging requests set in qpu noflush param
+in the profiled code.
+
+Example use:
+  vcgencmd set_logging level=0xc0
+  <command to profile>
+  sudo vcdbg log msg >& t.log
+  v3dusage.py t.log
+""")
+
+    argp.add_argument("logfile")
+    args = argp.parse_args()
+
+    do_logparse(args.logfile)
 
