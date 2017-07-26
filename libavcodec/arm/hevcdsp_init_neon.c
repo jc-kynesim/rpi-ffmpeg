603473f2f079c3c51e340d2e05babe2c859f23f8
John Cox <jc@kynesim.co.uk>
2017-07-26 14:49:19 +0100

    SAO Band C 32 10 written

--- libavcodec/arm/hevcdsp_init_neon.c
+++ libavcodec/arm/hevcdsp_init_neon.c
@@ -157,6 +157,12 @@ void ff_hevc_sao_band_c_neon_8(uint8_t *_dst, const uint8_t *_src,
                                   const int16_t *sao_offset_val_v, int sao_left_class_v,
                                   int width, int height);
 
+void ff_hevc_sao_band_c_32_neon_10(uint8_t *_dst, const uint8_t *_src,
+                                  ptrdiff_t stride_dst, ptrdiff_t stride_src,
+                                  const int16_t *sao_offset_val_u, int sao_left_class_u,
+                                  const int16_t *sao_offset_val_v, int sao_left_class_v,
+                                  int width, int height);
+
 void ff_hevc_sao_band_64_neon_8(uint8_t *_dst, uint8_t *_src, ptrdiff_t stride_dst, ptrdiff_t stride_src,
                                 int16_t *sao_offset_val, int sao_left_class, int width, int height);
 void ff_hevc_sao_band_64_neon_10(uint8_t *_dst, uint8_t *_src, ptrdiff_t stride_dst, ptrdiff_t stride_src,
@@ -610,6 +616,7 @@ av_cold void ff_hevcdsp_init_neon(HEVCDSPContext *c, const int bit_depth)
 #endif
         c->transform_4x4_luma          = ff_hevc_transform_luma_4x4_neon_10;
         c->sao_band_filter[4]          = ff_hevc_sao_band_64_neon_10;
+        c->sao_band_filter_c[2]        = ff_hevc_sao_band_c_32_neon_10;
     }
 
     assert(offsetof(MvField, mv) == 0);
