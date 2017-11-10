3b445c1213b75b69b0be3854b19238a6a887858b
John Cox <jc@kynesim.co.uk>
2017-11-10 12:58:52 +0000

    Readd VCCOREVER to defines

--- pi-util/conf_pi2.sh
+++ pi-util/conf_pi2.sh
@@ -5,6 +5,7 @@ RPI_OPT_VC=`pwd`/../firmware/opt/vc
 
 RPI_INCLUDES="-I$RPI_OPT_VC/include -I$RPI_OPT_VC/include/interface/vcos/pthreads -I$RPI_OPT_VC/include/interface/vmcs_host/linux"
 RPI_LIBDIRS="-L$RPI_TOOLROOT/lib -L$RPI_OPT_VC/lib"
+RPI_DEFINES="-D__VCCOREVER__=0x4000000"
 #RPI_KEEPS="-save-temps=obj"
 RPI_KEEPS=""
 
@@ -16,8 +17,8 @@ RPI_KEEPS=""
  --disable-thumb\
  --enable-mmal\
  --enable-rpi\
- --extra-cflags="-g $RPI_KEEPS $RPI_INCLUDES"\
- --extra-cxxflags="$RPI_INCLUDES"\
+ --extra-cflags="-g $RPI_KEEPS $RPI_DEFINES $RPI_INCLUDES"\
+ --extra-cxxflags="$RPI_DEFINES $RPI_INCLUDES"\
  --extra-ldflags="$RPI_LIBDIRS -Wl,-rpath=/opt/vc/lib,-rpath-link=$RPI_OPT_VC/lib,-rpath=/lib,-rpath=/usr/lib,-rpath-link=$RPI_TOOLROOT/lib,-rpath-link=$RPI_TOOLROOT/lib"\
  --extra-libs="-Wl,--start-group -lbcm_host -lmmal -lmmal_util -lmmal_core -lvcos -lvcsm -lvchostif -lvchiq_arm"\
  --cross-prefix=$RPI_TOOLROOT/bin/arm-linux-gnueabihf-
