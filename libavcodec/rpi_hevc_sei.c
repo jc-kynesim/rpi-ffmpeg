713422e3ce9fdabab2d0afc62703ff8f7cb9076f
John Cox <jc@kynesim.co.uk>
2017-12-19 12:38:18 +0000

    Import SEI overflow fix from head

--- libavcodec/rpi_hevc_sei.c
+++ libavcodec/rpi_hevc_sei.c
@@ -324,12 +324,16 @@ static int decode_nal_sei_message(GetBitContext *gb, HEVCSEIContext *s,
     av_log(logctx, AV_LOG_DEBUG, "Decoding SEI\n");
 
     while (byte == 0xFF) {
+       if (get_bits_left(gb) < 16 || payload_type > INT_MAX - 255)
+           return AVERROR_INVALIDDATA;
         byte          = get_bits(gb, 8);
         payload_type += byte;
     }
     byte = 0xFF;
     while (byte == 0xFF) {
-        byte          = get_bits(gb, 8);
+        if (get_bits_left(gb) < 8 + 8LL*payload_size)
+            return AVERROR_INVALIDDATA;
+         byte          = get_bits(gb, 8);
         payload_size += byte;
     }
     if (nal_unit_type == HEVC_NAL_SEI_PREFIX) {
