370c86b57275bbca908c80d3522766aa55db268c
John Cox <jc@kynesim.co.uk>
2018-02-27 11:43:28 +0000

    Make bin->h easier to work out where stuff is

--- pi-util/make_array.py
+++ pi-util/make_array.py
@@ -12,8 +12,12 @@ for file in sys.argv[1:]:
   print 'Converting',file
   with open(prefix+'.h','wb') as out:
     print >>out, 'static const unsigned char',name,'[] = {'
-    with open(file,'rb') as fd:  
+    with open(file,'rb') as fd:
+      i = 0
       for byte in fd.read():
-        print >>out, '%d,' % ord(byte)
+        print >>out, '0x%02x, ' % ord(byte),
+        i = i + 1
+        if i % 8 == 0:
+          print >>out, ' // %04x' % (i - 8)
     print >>out,'};'
 
