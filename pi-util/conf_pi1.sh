6075c3b3ec47b693ea2223fc8b0170b74fc31244
John Cox <jc@kynesim.co.uk>
2019-10-11 17:47:48 +0100

    Fix Pi1 (non-rpi) compile

--- pi-util/conf_pi1.sh
+++ pi-util/conf_pi1.sh
@@ -1,7 +1,7 @@
 echo "Configure for Pi1"
 
 RPI_TOOLROOT=`pwd`/../tools/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf
-RPI_OPT_VC=`pwd`/../firmware/opt/vc
+RPI_OPT_VC=`pwd`/../firmware/hardfp/opt/vc
 
 RPI_INCLUDES="-I$RPI_OPT_VC/include -I$RPI_OPT_VC/include/interface/vcos/pthreads -I$RPI_OPT_VC/include/interface/vmcs_host/linux"
 RPI_LIBDIRS="-L$RPI_TOOLROOT/lib -L$RPI_OPT_VC/lib"
@@ -15,6 +15,7 @@ RPI_KEEPS=""
  --target-os=linux\
  --disable-stripping\
  --enable-mmal\
+ --enable-shared\
  --extra-cflags="-g $RPI_KEEPS $RPI_INCLUDES"\
  --extra-cxxflags="$RPI_INCLUDES"\
  --extra-ldflags="$RPI_LIBDIRS -Wl,-rpath=/opt/vc/lib,-rpath-link=$RPI_OPT_VC/lib,-rpath=/lib,-rpath=/usr/lib,-rpath-link=$RPI_TOOLROOT/lib,-rpath-link=$RPI_TOOLROOT/lib"\
