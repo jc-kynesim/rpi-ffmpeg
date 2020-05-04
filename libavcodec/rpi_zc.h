f2e71035705ed88c9f7fc71653642c7a3ea4a6be
John Cox <jc@kynesim.co.uk>
2020-05-04 17:24:27 +0100

    Allow extraction of vc_handle from zc
    (cherry picked from commit 6185cc841530612783e49bc073ea83f68ba13b2a)

--- libavcodec/rpi_zc.h
+++ libavcodec/rpi_zc.h
@@ -76,16 +76,6 @@ AVRpiZcFrameGeometry av_rpi_zc_frame_geometry(
     const int format,
     const unsigned int video_width, const unsigned int video_height);
 
-// Replacement fn for avctx->get_buffer2
-// Should be set before calling avcodec_decode_open2
-//
-// N.B. in addition to to setting avctx->get_buffer2, avctx->refcounted_frames
-// must be set to 1 as otherwise the buffer info is killed before being returned
-// by avcodec_decode_video2.  Note also that this means that the AVFrame that is
-// returned must be manually derefed with av_frame_unref.  This should be done
-// after av_rpi_zc_ref has been called.
-int av_rpi_zc_get_buffer2(struct AVCodecContext *s, AVFrame *frame, int flags);
-
 // Generate a ZC reference to the buffer(s) in this frame
 // If the buffer doesn't appear to be one allocated by ZC
 // then the behaviour depends on maycopy:
@@ -100,6 +90,9 @@ AVRpiZcRefPtr av_rpi_zc_ref(void * const logging_context, const AVZcEnvPtr zc,
 // Get the vc_handle from the frame ref
 // Returns -1 if ref doesn't look valid
 int av_rpi_zc_vc_handle(const AVRpiZcRefPtr fr_ref);
+// Get the vcsm_handle from the frame ref
+// Returns 0 if ref doesn't look valid
+unsigned int av_rpi_zc_vcsm_handle(const AVRpiZcRefPtr fr_ref);
 // Get offset from the start of the memory referenced
 // by the vc_handle to valid data
 int av_rpi_zc_offset(const AVRpiZcRefPtr fr_ref);
@@ -119,8 +112,11 @@ void av_rpi_zc_unref(AVRpiZcRefPtr fr_ref);
 int av_rpi_zc_in_use(const struct AVCodecContext * const s);
 
 // Init ZC into a context
-// There is nothing magic in this fn - it just packages setting
-// get_buffer2 & get_buffer_context
+// Sets opaque, get_buffer2, thread_safe_callbacks
+// Use if you want to allocate your own pools and/or create ZC buffers for
+// all decoders
+// RPI HEVC decoders will allocate appropriate VCSM buffers which can be taken
+// apart by av_rpi_zc_xxx calls without this
 
 typedef AVBufferRef * av_rpi_zc_alloc_buf_fn_t(void * pool_env, size_t size,
                                                const AVRpiZcFrameGeometry * geo);
@@ -131,8 +127,6 @@ int av_rpi_zc_init2(struct AVCodecContext * const s,
                     av_rpi_zc_free_pool_fn_t * free_pool_fn);
 
 // Free ZC from a context
-// There is nothing magic in this fn - it just packages unsetting
-// get_buffer2 & get_buffer_context
 void av_rpi_zc_uninit2(struct AVCodecContext * const s);
 
 void av_rpi_zc_int_env_freep(AVZcEnvPtr * zc);
