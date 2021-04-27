913fb49bc4c171b6677afed599874f694653b512
John Cox <jc@kynesim.co.uk>
2021-04-27 12:31:16 +0100

    Add an unsand filter

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -507,6 +507,7 @@ extern const AVFilter ff_vf_trim;
 extern const AVFilter ff_vf_unpremultiply;
 extern const AVFilter ff_vf_unsharp;
 extern const AVFilter ff_vf_unsharp_opencl;
+extern const AVFilter ff_vf_unsand;
 extern const AVFilter ff_vf_untile;
 extern const AVFilter ff_vf_uspp;
 extern const AVFilter ff_vf_v360;
