2c1671aef1eff4052e5bf63731f72d19342cbc39
John Cox <jc@kynesim.co.uk>
2021-04-27 12:02:09 +0100

    Deal with the lack of trivial sand cropping

--- fftools/ffmpeg_filter.c
+++ fftools/ffmpeg_filter.c
@@ -2874,8 +2874,8 @@ static int send_frame(FilterGraph *fg, FilterGraphThread *fgt,
         break;
     case AVMEDIA_TYPE_VIDEO:
         if (ifp->format != frame->format ||
-            ifp->width  != frame->width ||
-            ifp->height != frame->height ||
+            ifp->width  != av_frame_cropped_width(frame) ||
+            ifp->height != av_frame_cropped_height(frame) ||
             ifp->color_space != frame->colorspace ||
             ifp->color_range != frame->color_range)
             need_reinit |= VIDEO_CHANGED;
