27e0c78a2df53fb2337bee4c383cdb58cbbc717e
John Cox <jc@kynesim.co.uk>
2021-04-28 10:16:39 +0100

    Add vout_drm

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -52,6 +52,7 @@ extern const FFOutputFormat ff_sndio_muxer;
 extern const AVInputFormat  ff_v4l2_demuxer;
 extern const FFOutputFormat ff_v4l2_muxer;
 extern const AVInputFormat  ff_vfwcap_demuxer;
+extern const FFOutputFormat ff_vout_drm_muxer;
 extern const AVInputFormat  ff_xcbgrab_demuxer;
 extern const FFOutputFormat ff_xv_muxer;
 
