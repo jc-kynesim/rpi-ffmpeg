8d3eb75fcb691443d0146b22ea60cca7d268e903
John Cox <jc@kynesim.co.uk>
2021-04-28 11:34:18 +0100

    Add vout_egl

--- libavdevice/alldevices.c
+++ libavdevice/alldevices.c
@@ -54,6 +54,7 @@ extern const FFOutputFormat ff_v4l2_muxer;
 extern const AVInputFormat  ff_vfwcap_demuxer;
 extern const FFOutputFormat ff_vout_drm_muxer;
 extern const AVInputFormat  ff_xcbgrab_demuxer;
+extern const FFOutputFormat ff_vout_egl_muxer;
 extern const FFOutputFormat ff_xv_muxer;
 
 /* external libraries */
