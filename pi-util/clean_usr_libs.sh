7bb34bb05bfd9eae4b51ff1c3d92e9a8d9403368
John Cox <jc@kynesim.co.uk>
2021-06-10 13:03:45 +0100

    Add small pi-util script to clean out installed ffmpeg libs

---
+++ pi-util/clean_usr_libs.sh
@@ -0,0 +1,16 @@
+set -e
+U=/usr/lib/arm-linux-gnueabihf
+rm -f $U/libavcodec.*
+rm -f $U/libavdevice.*
+rm -f $U/libavfilter.*
+rm -f $U/libavformat.*
+rm -f $U/libavresample.*
+rm -f $U/libavutil.*
+U=/usr/lib/arm-linux-gnueabihf/neon/vfp
+rm -f $U/libavcodec.*
+rm -f $U/libavdevice.*
+rm -f $U/libavfilter.*
+rm -f $U/libavformat.*
+rm -f $U/libavresample.*
+rm -f $U/libavutil.*
+
