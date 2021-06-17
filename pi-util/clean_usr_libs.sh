76cd7b194d37ef7071f9f0107372247819160857
John Cox <jc@kynesim.co.uk>
2021-06-17 13:08:36 +0100

    Clean out aarch64 too

--- pi-util/clean_usr_libs.sh
+++ pi-util/clean_usr_libs.sh
@@ -13,4 +13,11 @@ rm -f $U/libavfilter.*
 rm -f $U/libavformat.*
 rm -f $U/libavresample.*
 rm -f $U/libavutil.*
+U=/usr/lib/aarch64-linux-gnu
+rm -f $U/libavcodec.*
+rm -f $U/libavdevice.*
+rm -f $U/libavfilter.*
+rm -f $U/libavformat.*
+rm -f $U/libavresample.*
+rm -f $U/libavutil.*
 
