ff52e6053982154442dc948ed19bfee4396b0a95
John Cox <jc@kynesim.co.uk>
2022-06-30 15:59:23 +0000

    v4l2: Update H265 request for current API
    This works with v9 of the H265 patch set which hopefully will be the
    last one. Hevc controls extracted from patched v4l2-controls into
    hevc-ctrls-v4 - if HEVC controls found in the system v4l2-controls then
    those will be used instead.

--- libavcodec/v4l2_request_hevc.c
+++ libavcodec/v4l2_request_hevc.c
@@ -210,7 +210,11 @@ static int v4l2_request_hevc_init(AVCodecContext *avctx)
         goto fail4;
     }
 
-    if (V2(ff_v4l2_req_hevc, 3).probe(avctx, ctx) == 0) {
+    if (V2(ff_v4l2_req_hevc, 4).probe(avctx, ctx) == 0) {
+        av_log(avctx, AV_LOG_DEBUG, "HEVC API version 4 probed successfully\n");
+        ctx->fns = &V2(ff_v4l2_req_hevc, 4);
+    }
+    else if (V2(ff_v4l2_req_hevc, 3).probe(avctx, ctx) == 0) {
         av_log(avctx, AV_LOG_DEBUG, "HEVC API version 3 probed successfully\n");
         ctx->fns = &V2(ff_v4l2_req_hevc, 3);
     }
