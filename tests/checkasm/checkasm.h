4f63ad92ea7517a89630caa6fbd4eebfef10e444
John Cox <jc@kynesim.co.uk>
2023-07-01 18:43:57 +0000

    checkasm: Add tests for rpi_sand sand30 fns
    Something of a kludge for function selection as, at the moment, the
    rpi_sand fns don't have a jump table that we could use for selection.

--- tests/checkasm/checkasm.h
+++ tests/checkasm/checkasm.h
@@ -67,6 +67,7 @@ void checkasm_check_llviddspenc(void);
 void checkasm_check_nlmeans(void);
 void checkasm_check_opusdsp(void);
 void checkasm_check_pixblockdsp(void);
+void checkasm_check_rpi_sand(void);
 void checkasm_check_sbrdsp(void);
 void checkasm_check_synth_filter(void);
 void checkasm_check_sw_rgb(void);
