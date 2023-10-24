ee07ce6460f7629206e8f4861ca42ffdd6c72a4d
John Cox <jc@kynesim.co.uk>
2023-10-24 12:54:02 +0100

    dmabuf: Use vidbuf_cached for dmabuf allocation
    Gates usage to kernel 6.1.57 and later as that is when the rpivid iommu
    patch was merged.
    
    (cherry picked from commit 9a898f4ea127b30f1ca81eb98dfba3dd101db179)

--- libavcodec/v4l2_req_dmabufs.c
+++ libavcodec/v4l2_req_dmabufs.c
@@ -15,10 +15,11 @@
 #include "v4l2_req_dmabufs.h"
 #include "v4l2_req_utils.h"
 
-#define DMABUF_NAME1  "/dev/dma_heap/linux,cma"
-#define DMABUF_NAME2  "/dev/dma_heap/reserved"
-
 #define TRACE_ALLOC 0
+
+#ifndef __O_CLOEXEC
+#define __O_CLOEXEC 0
+#endif
 
 struct dmabufs_ctl;
 struct dmabuf_h;
@@ -297,23 +298,33 @@ struct dmabufs_ctl * dmabufs_ctl_ref(struct dmabufs_ctl * const dbsc)
 //
 // Alloc dmabuf via CMA
 
-static int ctl_cma_new(struct dmabufs_ctl * dbsc)
+static int ctl_cma_new2(struct dmabufs_ctl * dbsc, const char * const * names)
 {
-    while ((dbsc->fd = open(DMABUF_NAME1, O_RDWR)) == -1 &&
-           errno == EINTR)
-        /* Loop */;
-
-    if (dbsc->fd == -1) {
-        while ((dbsc->fd = open(DMABUF_NAME2, O_RDWR)) == -1 &&
+    for (; *names != NULL; ++names)
+    {
+        while ((dbsc->fd = open(*names, O_RDWR | __O_CLOEXEC)) == -1 &&
                errno == EINTR)
             /* Loop */;
-        if (dbsc->fd == -1) {
-            request_log("Unable to open either %s or %s\n",
-                    DMABUF_NAME1, DMABUF_NAME2);
-            return -1;
+        if (dbsc->fd != -1)
+        {
+            request_debug(NULL, "%s: Using dma_heap device %s\n", __func__, *names);
+            return 0;
         }
+        request_debug(NULL, "%s: Not using dma_heap device %s: %s\n", __func__, *names, strerror(errno));
     }
-    return 0;
+    request_log("Unable to open any dma_heap device\n");
+    return -1;
+}
+
+static int ctl_cma_new(struct dmabufs_ctl * dbsc)
+{
+    static const char * const names[] = {
+        "/dev/dma_heap/linux,cma",
+        "/dev/dma_heap/reserved",
+        NULL
+    };
+
+    return ctl_cma_new2(dbsc, names);
 }
 
 static void ctl_cma_free(struct dmabufs_ctl * dbsc)
@@ -321,7 +332,6 @@ static void ctl_cma_free(struct dmabufs_ctl * dbsc)
     if (dbsc->fd != -1)
         while (close(dbsc->fd) == -1 && errno == EINTR)
             /* loop */;
-
 }
 
 static int buf_cma_alloc(struct dmabufs_ctl * const dbsc, struct dmabuf_h * dh, size_t size)
@@ -347,6 +357,10 @@ static int buf_cma_alloc(struct dmabufs_ctl * const dbsc, struct dmabuf_h * dh, 
 
     dh->fd = data.fd;
     dh->size = (size_t)data.len;
+
+//    fprintf(stderr, "%s: size=%#zx, ftell=%#zx\n", __func__,
+//            dh->size, (size_t)lseek(dh->fd, 0, SEEK_END));
+
     return 0;
 }
 
@@ -364,7 +378,32 @@ static const struct dmabuf_fns dmabuf_cma_fns = {
 
 struct dmabufs_ctl * dmabufs_ctl_new(void)
 {
-    request_debug(NULL, "Dmabufs using CMA\n");;
+    request_debug(NULL, "Dmabufs using CMA\n");
     return dmabufs_ctl_new2(&dmabuf_cma_fns);
+}
+
+static int ctl_cma_new_vidbuf_cached(struct dmabufs_ctl * dbsc)
+{
+    static const char * const names[] = {
+        "/dev/dma_heap/vidbuf_cached",
+        "/dev/dma_heap/linux,cma",
+        "/dev/dma_heap/reserved",
+        NULL
+    };
+
+    return ctl_cma_new2(dbsc, names);
+}
+
+static const struct dmabuf_fns dmabuf_vidbuf_cached_fns = {
+    .buf_alloc  = buf_cma_alloc,
+    .buf_free   = buf_cma_free,
+    .ctl_new    = ctl_cma_new_vidbuf_cached,
+    .ctl_free   = ctl_cma_free,
+};
+
+struct dmabufs_ctl * dmabufs_ctl_new_vidbuf_cached(void)
+{
+    request_debug(NULL, "Dmabufs using Vidbuf\n");
+    return dmabufs_ctl_new2(&dmabuf_vidbuf_cached_fns);
 }
 
