9a898f4ea127b30f1ca81eb98dfba3dd101db179
John Cox <jc@kynesim.co.uk>
2023-10-24 12:54:02 +0100

    dmabuf: Use vidbuf_cached for dmabuf allocation
    Gates usage to kernel 6.1.57 and later as that is when the rpivid iommu
    patch was merged.

--- libavcodec/v4l2_req_dmabufs.h
+++ libavcodec/v4l2_req_dmabufs.h
@@ -7,6 +7,7 @@ struct dmabufs_ctl;
 struct dmabuf_h;
 
 struct dmabufs_ctl * dmabufs_ctl_new(void);
+struct dmabufs_ctl * dmabufs_ctl_new_vidbuf_cached(void);
 void dmabufs_ctl_unref(struct dmabufs_ctl ** const pdbsc);
 struct dmabufs_ctl * dmabufs_ctl_ref(struct dmabufs_ctl * const dbsc);
 
