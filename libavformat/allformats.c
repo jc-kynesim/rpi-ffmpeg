795522c0a994688d1eb8eb270ef913e4d75cf233
John Cox <jc@kynesim.co.uk>
2025-01-06 19:25:15 +0000

    conformenc: Add an output mux for conformance testing
    Add a mux that automagically selects the correct YUV type for
    conformance testing and creates an MD5 or YUV file from any input. This
    fixes problems where conformance streams change pix fmt midway making
    it impossible to create a command line with the correct conversions.

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -116,6 +116,7 @@ extern const AVInputFormat  ff_codec2raw_demuxer;
 extern const AVOutputFormat ff_codec2raw_muxer;
 extern const AVInputFormat  ff_concat_demuxer;
 extern const AVOutputFormat ff_crc_muxer;
+extern const AVOutputFormat ff_conform_muxer;
 extern const AVInputFormat  ff_dash_demuxer;
 extern const AVOutputFormat ff_dash_muxer;
 extern const AVInputFormat  ff_data_demuxer;
