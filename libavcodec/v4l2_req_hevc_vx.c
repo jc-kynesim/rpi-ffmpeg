e2e2406c6d0bc077d74089d25670808860b2646e
John Cox <jc@kynesim.co.uk>
2025-01-16 17:09:50 +0000

    v4l2_req: Relax device checks based on new understanding

--- libavcodec/v4l2_req_hevc_vx.c
+++ libavcodec/v4l2_req_hevc_vx.c
@@ -903,17 +903,6 @@ static int v4l2_request_hevc_decode_slice(AVCodecContext *avctx, const uint8_t *
         }
     }
 
-    if (ctx->decode_mode == V4L2_STATELESS_HEVC_DECODE_MODE_FRAME_BASED) {
-        if (rd->slices == NULL) {
-            if ((rd->slices = av_mallocz(sizeof(*rd->slices))) == NULL)
-                return AVERROR(ENOMEM);
-            rd->slices->ptr = buffer;
-            rd->num_slices = 1;
-        }
-        rd->slices->len = buffer - rd->slices->ptr + size;
-        return 0;
-    }
-
     if ((rv = slice_add(rd)) != 0)
         return rv;
 
@@ -1141,11 +1130,7 @@ probe(AVCodecContext * const avctx, V4L2RequestContextHEVC * const ctx)
 
     mediabufs_ctl_query_ext_ctrls(ctx->mbufs, qc, noof_ctrls);
     i = 0;
-#if HEVC_CTRLS_VERSION >= 4
-    // Skip slice check if no slice mode
-    if (qc[1].type != 0 && !ctrl_valid(qc + 1, V4L2_STATELESS_HEVC_DECODE_MODE_SLICE_BASED))
-        i = 1;
-#else
+#if HEVC_CTRLS_VERSION < 4
     // Fail frame mode silently for anything prior to V4
     if (qc[1].type == 0 || !ctrl_valid(qc + 1, V4L2_STATELESS_HEVC_DECODE_MODE_SLICE_BASED))
         return AVERROR(EINVAL);
