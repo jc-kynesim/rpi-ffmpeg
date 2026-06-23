89275ad059740f06bfe58cf60407e79c567bc828
John Cox <jc@kynesim.co.uk>
2026-06-23 15:34:21 +0100

    testfilt: Add tests for H265 to H264
    Test code is still a work in progress and only works on Pi4
    but is handy to have

--- pi-util/testfilt.py
+++ pi-util/testfilt.py
@@ -34,18 +34,13 @@ def validate(validators, flog):
         for v in validators:
             v.scanline(line)
 
-    ok = True
     for v in validators:
         if not v.isok():
-            ok = False
-            # complain
-            print("Test failed")
+            return False
+    return True
 
-    if ok:
-        print("OK")
-    return ok
+def runtest2(name, ffmpeg, args, suffix, validators):
 
-def runtest(name, ffmpeg, args, suffix, validators):
     log_root = os.path.join("/tmp", "testfilt", name)
     ofilename = os.path.join(log_root, name + suffix)
 
@@ -66,18 +61,70 @@ def runtest(name, ffmpeg, args, suffix, validators):
     flog = open(os.path.join(log_root, name + ".log"), "rt")
     return validate(validators, flog)
 
+def runtest(name, ffmpeg, args, suffix, validators):
+
+    print (f'==== {name}: ', end="")
+    sys.stdout.flush()
+
+    rv = runtest2(name, ffmpeg, args, suffix, validators)
+
+    if rv:
+        print("ok")
+    else:
+        print("FAIL")
+
+    return rv
+
+
 def sayok(log_root, flog):
     print("Woohoo")
     return True
 
-if __name__ == '__main__':
-
+def main():
     argp = argparse.ArgumentParser(description="FFmpeg filter tester")
-    argp.add_argument("--ffmpeg", default="./ffmpeg", help="ffmpeg exec name")
+    argp.add_argument("--ffmpeg", default="./ffmpeg", help="ffmpeg exec name or build directory")
+    argp.add_argument("--test_root", default="../streams", help="directory with 3mbit h264/265 jellyfish files")
     args = argp.parse_args()
 
-    runtest("ATest", args.ffmpeg, ["-v", "verbose", "-no_cvt_hw", "-an", "-c:v", "h264_v4l2m2m", "-i",
-                                   "/home/johncox/server/TestMedia/Sony/jellyfish-10-mbps-hd-h264.mkv",
-#                                    "/home/jc/rpi/streams/jellyfish-3-mbps-hd-h264.mkv",
+    if os.path.isdir(args.ffmpeg):
+        args.ffmpeg = os.path.join(args.ffmpeg, "ffmpeg")
+    if not os.path.isfile(args.ffmpeg):
+        print("FFmpeg file '%s' not found" % args.ffmpeg)
+        return 2
+
+    if not os.path.isdir(args.test_root):
+        print("Jellyfish source dir '%s' not found" % args.test_root)
+        return 2
+    h264_src = os.path.join(args.test_root, "jellyfish-3-mbps-hd-h264.mkv")
+    h265_src = os.path.join(args.test_root, "jellyfish-3-mbps-hd-hevc.mkv")
+    if not os.path.isfile(h264_src):
+        print(f"Jellyfish 3Mbit H264 source file {h264_src} not found")
+    if not os.path.isfile(h265_src):
+        print(f"Jellyfish 3Mbit H265 source file {h265_src} not found")
+
+    rv = runtest("H264H264", args.ffmpeg, ["-v", "verbose", "-no_cvt_hw", "-an", "-c:v", "h264_v4l2m2m",
+                                   "-i", h264_src,
                                    "-c:v", "h264_v4l2m2m", "-b:v", "2M"], ".mkv",
             [valid_regex(r'Output stream #0:0 \(video\): 900 frames encoded; 900 packets muxed')])
+
+    rv = runtest("H265H264", args.ffmpeg, ["-v", "verbose", "-no_cvt_hw", "-an",
+                                   "-hwaccel", "drm", "-init_hw_device", "drm:,v4l2fmts=NC12", "-c:v", "hevc",
+                                   "-i", h265_src,
+                                   "-c:v", "h264_v4l2m2m", "-b:v", "2M"], ".mkv",
+            [valid_regex(r'Output stream #0:0 \(video\): 900 frames encoded; 900 packets muxed')])
+
+    rv = runtest("H265ScaleH264", args.ffmpeg, ["-v", "verbose", "-no_cvt_hw", "-an",
+                                   "-hwaccel", "drm", "-init_hw_device", "drm:,v4l2fmts=NC12", "-c:v", "hevc",
+                                   "-i", h265_src,
+                                   "-vf", "scale_v4l2m2m=w=1280:h=720:format=nv12",
+                                   "-c:v", "h264_v4l2m2m", "-b:v", "2M"], ".mkv",
+            [valid_regex(r'Output stream #0:0 \(video\): 900 frames encoded; 900 packets muxed')])
+
+
+    if not rv:
+        return 3
+    return 0
+
+if __name__ == '__main__':
+    exit(main())
+
